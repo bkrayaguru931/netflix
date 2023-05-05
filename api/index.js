9be8d8a6cfb33e7056d4919b05fac54843d03de8
babykrishnarayaguru <babykrishnarayaguru@gmail.com>
2023-05-05 23:57:14 +0530

    added JWT authentication

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
 
 dotenv.config();
@@ -9,11 +10,16 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+
   })
   .then(() => console.log("DB Connection Successfull"))
   .catch((err) => {
     console.error(err);
   });
+
+  app.use(express.json());
+
+  app.use("/api/auth", authRoute);
 
 
 
---
+++ api/routes/lists.js
@@ -0,0 +1,2 @@
+const router = require("express").Router();
+const List = require("../models/List");
